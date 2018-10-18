63cc7a1ef02e757708365c2ad8e63fbc81edc0e0
mostafaebrahimi.me@gmail.com <mostafaebrahimi.me@gmail.com>
2018-10-18 12:04:07 +0330

    config react-redux-actions

--- src/containers/Auth/LoginScreen.js
+++ src/containers/Auth/LoginScreen.js
@@ -1,99 +1,120 @@
-import React, { Component } from 'react'
-import { View, Text, Image, Dimensions, TouchableOpacity } from 'react-native'
-import BackgroundImage from '../../components/BackgroundImage'
-import { Toast } from 'antd-mobile-rn'
-import { Colors, Images, Strings } from '../../config'
-import style from './Style'
-import { Button, Item, Input, Title, Subtitle } from 'native-base'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { View, Text, Image, Dimensions, TouchableOpacity } from "react-native";
+import BackgroundImage from "../../components/BackgroundImage";
+import { Toast } from "antd-mobile-rn";
+import { Colors, Images, Strings } from "../../config";
+import style from "./Style";
+import { Button, Item, Input, Title, Subtitle } from "native-base";
+import { connect } from "react-redux";
+import {
+  increamentCounter,
+  decreamentCounter
+} from "../../redux/reducers/Auth";
 
-const mapStateToProps = (state) => {
-    return {
+const mapStateToProps = state => {
+  return {
+    mystate: state
+  };
+};
 
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-
-    }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    inc: () => dispatch(increamentCounter())
+  };
+};
 
 class LoginScreen extends Component {
-    static navigationOptions = {
-        header: null
-    }
+  static navigationOptions = {
+    header: null
+  };
 
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            phone: '',
-            isLoading: false
-        }
-        this.changeInputValue = this.changeInputValue.bind(this);
-        this.submitPhoneNumber = this.submitPhoneNumber.bind(this);
-    }
+    this.state = {
+      phone: "",
+      isLoading: false
+    };
+    this.changeInputValue = this.changeInputValue.bind(this);
+    this.submitPhoneNumber = this.submitPhoneNumber.bind(this);
+  }
 
+  componentDidMount() {
+    console.log(increamentCounter());
+    console.log(decreamentCounter());
+    this.props.inc();
+  }
 
-    submitPhoneNumber() {
-        let that = this;
-        Toast.info("this is toast");
-        console.log(this.state)
-        this.setState({ isLoading: true })
-        setTimeout(function () {
-            that.setState({ isLoading: false })
-            that.props.navigation.navigate('Register')
-        }, 5000)
-    }
+  submitPhoneNumber() {
+    let that = this;
+    Toast.info("this is toast");
+    console.log(this.state);
+    this.setState({ isLoading: true });
+    setTimeout(function() {
+      that.setState({ isLoading: false });
+      that.props.navigation.navigate("Register");
+    }, 5000);
+  }
 
-    changeInputValue(value) {
-        this.setState({ phone: value })
-    }
+  changeInputValue(value) {
+    this.setState({ phone: value });
+  }
 
-    changePhoneNumber(text) {
-        if (text.length <= 11)
-            this.setState({ phone: text })
-        else Toast.fail('Phone number length  must be less than 11')
-    }
+  changePhoneNumber(text) {
+    if (text.length <= 11) this.setState({ phone: text });
+    else Toast.fail("Phone number length  must be less than 11");
+  }
 
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <BackgroundImage>
-                    <View style={[style.darkBoxWithOpacity]}>
-                        <View style={style.logoContainer}>
-                            <Image source={Images.splash.logo} style={style.logo} ></Image>
-                            <Text style={style.AppName}>{Strings.APP_NAME}</Text>
-                        </View>
-                        <View style={[style.textInputContainer]}>
-                            <View >
-                                <Title>{Strings.splash.SIGN_UP_TO_MOON_LESSON}</Title>
-                                <Subtitle>{Strings.splash.Please_Enter_Your_Phone_Number_to_Continue}</Subtitle>
-                            </View>
-                            <Item style={style.ItemStyle}>
-                                <Input placeholderTextColor={Colors.placeHolderColor}
-                                    placeholder={Strings.splash.usernameInput}
-                                    style={style.textInput} />
-                            </Item>
-                            <Item style={style.ItemStyle}>
-                                <Input secureTextEntry={true}
-                                    placeholderTextColor={Colors.placeHolderColor}
-                                    placeholder={Strings.splash.passwordInput}
-                                    style={style.textInput} />
-                            </Item>
-                            <TouchableOpacity style={[style.submitButton]}
-                                loading={this.state.isLoading}
-                                onPress={this.submitPhoneNumber}>
-                                <Text style={style.submitButtonText}>{Strings.splash.submit}</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </BackgroundImage>
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <BackgroundImage>
+          <View style={[style.darkBoxWithOpacity]}>
+            <View style={style.logoContainer}>
+              <Image source={Images.splash.logo} style={style.logo} />
+              <Text style={style.AppName}>{Strings.APP_NAME}</Text>
             </View>
-        )
-    }
+            <View style={[style.textInputContainer]}>
+              <View>
+                <Title>{Strings.splash.SIGN_UP_TO_MOON_LESSON}</Title>
+                <Subtitle>
+                  {Strings.splash.Please_Enter_Your_Phone_Number_to_Continue}
+                </Subtitle>
+              </View>
+              <Item style={style.ItemStyle}>
+                <Input
+                  placeholderTextColor={Colors.placeHolderColor}
+                  placeholder={Strings.splash.usernameInput}
+                  style={style.textInput}
+                />
+              </Item>
+              <Item style={style.ItemStyle}>
+                <Input
+                  secureTextEntry={true}
+                  placeholderTextColor={Colors.placeHolderColor}
+                  placeholder={Strings.splash.passwordInput}
+                  style={style.textInput}
+                />
+              </Item>
+              <TouchableOpacity
+                style={[style.submitButton]}
+                loading={this.state.isLoading}
+                onPress={this.submitPhoneNumber}
+              >
+                <Text style={style.submitButtonText}>
+                  {Strings.splash.submit}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </BackgroundImage>
+      </View>
+    );
+  }
 }
 
-const LoginScreenConnect = connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
-export default LoginScreenConnect
+const LoginScreenConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginScreen);
+export default LoginScreenConnect;
--- src/redux/reducers/Auth.js
+++ src/redux/reducers/Auth.js
@@ -1,14 +1,31 @@
-import { LOGIN } from '../types/Auth'
-
+import { createAction, handleActions } from "redux-actions";
 const initialState = {
+  username: undefined,
+  password: undefined,
+  isFetching: false,
+  counter: 0
+};
 
-}
+export const increamentCounter = createAction("COUNTER_INCREAMENT");
+export const decreamentCounter = createAction("COUNTER_DECREAMENT");
+export const login = createAction("LOGIN");
 
-export default function (state = initialState, action) {
-    switch (action.type) {
-        case LOGIN:
-            return state
-        default:
-            return state;
+export default (AuthActions = handleActions(
+  {
+    [increamentCounter](state) {
+      return { ...state, counter: state.counter + 1 };
+    },
+    [decreamentCounter](state) {
+      return { ...state, counter: state.counter - 1 };
+    },
+    [login](state, payload) {
+      return {
+        ...state,
+        username: payload.username,
+        password: payload.password,
+        isFetching: true
+      };
     }
-}
+  },
+  initialState
+));
